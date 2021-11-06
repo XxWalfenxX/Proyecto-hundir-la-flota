d83d559c57be943e6084717d7725baea18c8d187
firagonb3 <malebl1@bemen3.cat>
2021-11-06 10:00:20 +0100

    master

--- UndirLaFlota.java
+++ UndirLaFlota.java
@@ -4,12 +4,12 @@ public class UndirLaFlota {
     // final int iTurnos = 2;
 
     public static final int iTurnos = 1;
-    public static final int iBarcos = 20;
+    public static final int iBarcosTocados = 20;
     private static Process start;
 
     public static void main(String[] args) {
         ProcessBuilder CLS = new ProcessBuilder("cmd", "/c", "cls"); // Esta sentencia es para crear un proceso de cmd.
-        Scanner sc = new Scanner(System.in); 
+        Scanner sc = new Scanner(System.in);
 
         int iPosicionX = 0, iPosicionY = 0, iTurnosPlayer = 0, iTocadosContador = 0;
         String iCordenadas = "";
@@ -19,16 +19,16 @@ public class UndirLaFlota {
         // En el Array emos determidado los caracteres m y B, el caracter m representa
         // el mar y el caracter B representan los barcos.
         char[][] cTablero = { 
-                { 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'B', },
-                { 'm', 'B', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'B', },
-                { 'm', 'B', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'B', },
-                { 'm', 'B', 'm', 'B', 'B', 'B', 'B', 'B', 'm', 'm', },
-                { 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', },
-                { 'm', 'm', 'm', 'B', 'B', 'm', 'm', 'B', 'm', 'm', },
-                { 'm', 'B', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', },
-                { 'm', 'm', 'm', 'm', 'm', 'B', 'm', 'm', 'B', 'm', },
-                { 'B', 'B', 'm', 'B', 'm', 'm', 'm', 'm', 'B', 'm', },
-                { 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', } };
+            { 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'B', },
+            { 'm', 'B', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'B', },
+            { 'm', 'B', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'B', },
+            { 'm', 'B', 'm', 'B', 'B', 'B', 'B', 'B', 'm', 'm', },
+            { 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', },
+            { 'm', 'm', 'm', 'B', 'B', 'm', 'm', 'B', 'm', 'm', },
+            { 'm', 'B', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', },
+            { 'm', 'm', 'm', 'm', 'm', 'B', 'm', 'm', 'B', 'm', },
+            { 'B', 'B', 'm', 'B', 'm', 'm', 'm', 'm', 'B', 'm', },
+            { 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', } };
 
         // Este do while representa el loop principal del juego, terminara cuando agotes
         // todos los turnos o undas todos los barcos.
@@ -36,9 +36,9 @@ public class UndirLaFlota {
             do {
                 try { // el try es un metodo para preveer las Excepciones
                     start = CLS.inheritIO().start(); // Esta sentencia es para al macenar el proceso con la funcion de
-                                                    // inicializacion en start que es una variable privada.
+                                                     // inicializacion en start que es una variable privada.
                     start.waitFor(); // Esta sentencia lo que hace es inicializar el proceso en todas las lineas
-                                    // necesarias.
+                                     // necesarias.
                 } catch (Exception e) {
 
                 }
@@ -51,15 +51,15 @@ public class UndirLaFlota {
 
                 System.out.println("    1  2  3  4  5  6  7  8  9  10");
                 for (iPosicionX = 0; iPosicionX < cTablero.length; iPosicionX++) { // Al comenzar el for lo que hace es
-                                                                                // poner iPosicionX en 0 para que en
-                                                                                // pieze desde la primera fila asta que
-                                                                                // iPosicionX sea menor a la longitud
-                                                                                // del cTablero.
+                                                                                   // poner iPosicionX en 0 para que
+                                                                                   // en pieze desde la primera fila
+                                                                                   // asta que iPosicionX sea menor a la
+                                                                                   // longitud del cTablero.
 
                     // Este prin imprime en pantalla los caracteres de la A a la J.
                     // El Character.forDigit() es un metodo que convierte cualquier entero de base
                     // 10 a la base que le indiques, como el tabledo contiene 10 filas lo que emos
-                    // es echo es pner el numero de filas mas 10 que es iPosicionX + 10.
+                    // echo es pner el numero de filas mas 10 que es iPosicionX + 10.
                     System.out.print(" " + Character.forDigit(iPosicionX + 10, 20) + " ");
 
                     // Dentro del for se ejecuta otro for que recorre las colupnas del Array.
@@ -78,24 +78,25 @@ public class UndirLaFlota {
                 } // El for terminara cuandao haya recorido toda las colupnas
 
                 System.out.println("----------------------------------");
-                System.out.println("Turnos: "  + (iTurnosPlayer+1) + " de " + (iTurnos + 1));
+                System.out.println("Turnos: " + (iTurnosPlayer + 1) + " de " + (iTurnos + 1));
                 // Este do while sirve para cuando el usuario tenga que poner las cordenads se
                 // repita en caso de que sean erroneas.
                 do {
 
-                        System.out.print("Pon las cordenadas: ");
-                        iCordenadas = sc.nextLine(); // en iCordenadas se almacenadan las cordenadas de la siguiente manera:
-                                                    // A1, G5, etc...
+                    System.out.print("Pon las cordenadas: ");
+                    iCordenadas = sc.nextLine(); // en iCordenadas se almacenadan las cordenadas de la siguiente manera:
+                                                 // A1, G5, etc...
 
                     // En iPosicionX lo que hacemos es coger el primer caracter del String con el
                     // metodo charAt() luego lo convertimos a entero con la classe
                     // Character usando el metodo getNumericValue() luego le restamos 10 ya que al
                     // pasar por ejemplo el carachter A se convierte en 10 y al restarle 10 pasa a
                     // 0. otros ejemplos seria B = 1, C = 3, etc...
-                    try {
+
+                    try { // con el try catch estamos comprobando si puede dar una excepcion en caso de
+                          // que los valores no sean correctosm, si aparece una excepcion ejecutara el catch en vez del try.
                         iPosicionX = Character.getNumericValue(iCordenadas.charAt(0)) - 10;
                     } catch (Exception e) {
-                        //TODO: handle exception
                         System.out.println("!!! Cordenadas incorrectas: Valor erroneo !!!");
                     }
 
@@ -105,11 +106,10 @@ public class UndirLaFlota {
                     try {
                         iPosicionY = Integer.parseInt(iCordenadas.substring(1)) - 1;
                     } catch (Exception e) {
-                        //TODO: handle exception
                         System.out.println("!!! Cordenadas incorrectas: Conversion erronea !!!");
-                       
+
                     }
-                    
+
                     // Con este if se comprueba que la posicon X y Y no sea mayor 10 ni inferior a 0.
                     // Si se cumple saldra del do while.
                     if (iPosicionX >= 0 & iPosicionX < cTablero.length & iPosicionY >= 0 & iPosicionY < cTablero.length) {
@@ -125,39 +125,49 @@ public class UndirLaFlota {
                 if (cTablero[iPosicionX][iPosicionY] == 'B') {
                     cTablero[iPosicionX][iPosicionY] = 'T';
                     iTocadosContador++;
-                    if (iTocadosContador == iBarcos) {
-                        break;
-                    }
+
                 } else if (cTablero[iPosicionX][iPosicionY] == 'm') {
                     cTablero[iPosicionX][iPosicionY] = 'V';
-                    break;
+
                 }
 
-            } while (true);
-            
-            
+            } while (iTocadosContador != iBarcosTocados || cTablero[iPosicionX][iPosicionY] != 'm');
+
+            // con este if conprobamos que los turnos del jugador sean iguales alos turnos
+            // del juego si se cumple la condicion imprime ne pantalla el tabledo mostrando
+            // la uvicacion de los barcos y mandando te un mensaje de que as perdido y
+            // finaliza el juego.
             if (iTurnosPlayer == iTurnos) {
                 System.out.println("    1  2  3  4  5  6  7  8  9  10");
+
                 for (iPosicionX = 0; iPosicionX < cTablero.length; iPosicionX++) {
                     System.out.print(" " + Character.forDigit(iPosicionX + 10, 20) + " ");
                     for (iPosicionY = 0; iPosicionY < cTablero.length; iPosicionY++) {
                         System.out.print("[" + cTablero[iPosicionX][iPosicionY] + "]");
-                    } 
-                    System.out.println(""); 
+
+                    }
+                    System.out.println("");
                 }
 
                 System.out.println("----------------------------------");
                 System.out.println("!!! Game Over !!!");
                 System.out.println("!!! Perdiste boludo !!!");
                 System.exit(0);
-            } else if (iTocadosContador == iBarcos) {
+
+                // con el if else se comprueba si los barcos tocados es igual al numero de
+                // tocados del juego se muester en pantalla els tablero y te salte un mensaje de
+                // que as ganado y finaliza el juego.
+            } else if (iTocadosContador == iBarcosTocados) {
                 System.out.println("    1  2  3  4  5  6  7  8  9  10");
+
                 for (iPosicionX = 0; iPosicionX < cTablero.length; iPosicionX++) {
                     System.out.print(" " + Character.forDigit(iPosicionX + 10, 20) + " ");
+
                     for (iPosicionY = 0; iPosicionY < cTablero.length; iPosicionY++) {
                         System.out.print("[" + cTablero[iPosicionX][iPosicionY] + "]");
-                    } 
-                    System.out.println(""); 
+
+                    }
+                    System.out.println("");
                 }
 
                 System.out.println("----------------------------------");
@@ -165,7 +175,8 @@ public class UndirLaFlota {
                 System.out.println("!!! boludo !!!");
                 System.exit(0);
             }
-            iTurnosPlayer++;
+            iTurnosPlayer++; // con esta linea sumas los turnos del jugador cadevez que llehas al final del
+                             // bucle.
         } while (true);
     }
 }
