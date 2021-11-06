a8cf5f2a5aae8b0890cc831e0dedc7be9b9113ea
firagonb3 <malebl1@bemen3.cat>
2021-11-06 12:06:13 +0100

    master

--- UndirLaFlota.java
+++ UndirLaFlota.java
@@ -17,16 +17,18 @@ public class UndirLaFlota {
         // cTablero, loemos mapedo de manera manual.
         // En el Array emos determidado los caracteres m y B, el caracter m representa
         // el mar y el caracter B representan los barcos.
-        char[][] cTablero = { { 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'B', },
-                { 'm', 'B', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'B', },
-                { 'm', 'B', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'B', },
-                { 'm', 'B', 'm', 'B', 'B', 'B', 'B', 'B', 'm', 'm', },
-                { 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', },
-                { 'm', 'm', 'm', 'B', 'B', 'm', 'm', 'B', 'm', 'm', },
-                { 'm', 'B', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', },
-                { 'm', 'm', 'm', 'm', 'm', 'B', 'm', 'm', 'B', 'm', },
-                { 'B', 'B', 'm', 'B', 'm', 'm', 'm', 'm', 'B', 'm', },
-                { 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', } };
+        char[][] cTablero = { 
+            { 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'B', },
+            { 'm', 'B', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'B', },
+            { 'm', 'B', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'B', },
+            { 'm', 'B', 'm', 'B', 'B', 'B', 'B', 'B', 'm', 'm', },
+            { 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', },
+            { 'm', 'm', 'm', 'B', 'B', 'm', 'm', 'B', 'm', 'm', },
+            { 'm', 'B', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', },
+            { 'm', 'm', 'm', 'm', 'm', 'B', 'm', 'm', 'B', 'm', },
+            { 'B', 'B', 'm', 'B', 'm', 'm', 'm', 'm', 'B', 'm', },
+            { 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', } 
+        };
 
         // Este do while representa el loop principal del juego, terminara cuando agotes
         // todos los turnos o undas todos los barcos.
@@ -76,8 +78,7 @@ public class UndirLaFlota {
                 } // El for terminara cuandao haya recorido toda las colupnas
 
                 System.out.println("----------------------------------");
-                System.out.println("Turnos: " + (iTurnosPlayer + 1) + " de " + (iTurnos + 1) + " : tocados: "
-                        + iTocadosContador + " de " + iBarcosTocados);
+                System.out.println("Turnos: " + (iTurnosPlayer + 1) + " de " + (iTurnos + 1) + " : tocados: " + iTocadosContador + " de " + iBarcosTocados);
                 // Este do while sirve para cuando el usuario tenga que poner las cordenads se
                 // repita en caso de que sean erroneas.
                 do {
@@ -110,11 +111,9 @@ public class UndirLaFlota {
 
                     }
 
-                    // Con este if se comprueba que la posicon X y Y no sea mayor 10 ni inferior a
-                    // 0.
+                    // Con este if se comprueba que la posicon X y Y no sea mayor 10 ni inferior a 0.
                     // Si se cumple saldra del do while.
-                    if (iPosicionX >= 0 & iPosicionX < cTablero.length & iPosicionY >= 0
-                            & iPosicionY < cTablero.length) {
+                    if (iPosicionX >= 0 & iPosicionX < cTablero.length & iPosicionY >= 0 & iPosicionY < cTablero.length) {
                         break;
                     } else {
                         System.out.println("!!! Cordenadas incorrectas: Fuera de rango !!!");
