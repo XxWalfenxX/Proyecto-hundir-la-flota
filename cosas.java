b1d5cd24bf78b80bd7b1d866d7a20b0d89f5c8aa
firagonb3 <malebl1@bemen3.cat>
2021-11-19 13:09:40 +0100

    master

--- cosas.java
+++ cosas.java
@@ -7,6 +7,16 @@ import java.util.Scanner;
  * @author MarcLecha
  */
 public class cosas {
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    public static final String RESET = "\u001B[0m";
 
     private static ProcessBuilder CLS = new ProcessBuilder("cmd", "/c", "cls"); // Esta sentencia es para crear un proceso de cmd.
     private static Process start;
@@ -22,6 +32,7 @@ public class cosas {
     public static final char cBarco = 'B';
     public static final char cTocados = 'T';
     public static final char cVacio = 'A';
+    public static final char cHundido = 'H';
 
     /**
      * @param args the command line arguments
@@ -35,9 +46,10 @@ public class cosas {
         int iRandom, iRandom2, iBarcosNivel1Contador = 0, iBarcosNivel2Contador = 0, iBarcosNivel3Contador = 0, iBarcosNivel4Contador = 0;
         boolean bTestArriba = false, bTestAbajo = false, bTestIzquierda = false, bTestDerecha = false;
 
-        String[] aBarcosNivel3Posiciones = new String[3];
-        int iBarcosTocadosNivel3 = 0;
+        String[] aBarcosNivel3Posiciones = new String[3*iBarcosNivel3];
+        int iBarcosTocadosNivel3 = 0, iBarcosTocados2Nivel3 = 0;
         boolean iBarcosNivel3Undido = false;
+        boolean iBarcosNivel3Undido2 = false;
 
         for (iPosicionX = 0; iPosicionX < cTablero.length; iPosicionX++) {
             for (iPosicionY = 0; iPosicionY < cTablero.length; iPosicionY++) {
@@ -99,19 +111,31 @@ public class cosas {
                                     cTablero[iPosicionX][iPosicionY] = cBarco ;
                                     cTablero[iPosicionX][iPosicionY+1] = cBarco ;
                                     cTablero[iPosicionX][iPosicionY+2] = cBarco ;
-                                    iBarcosNivel3Contador++;
 
                                     //Almacenar Posicion del barco de nivel 3 orizontal
                                     //------------------------------------------------------------------------------------------
+                                    
                                     if (iBarcosNivel3Contador == 0) {
+                                        System.out.println("");
                                         aBarcosNivel3Posiciones[0] = Integer.toString(iPosicionY) + Integer.toString(iPosicionX);
                                         aBarcosNivel3Posiciones[1] = Integer.toString(iPosicionY + 1) + Integer.toString(iPosicionX);
                                         aBarcosNivel3Posiciones[2] = Integer.toString(iPosicionY + 2) + Integer.toString(iPosicionX);
                                     } else {
-                                        aBarcosNivel3Posiciones[0+(iBarcosNivel3Contador + 2)] = Integer.toString(iPosicionY) + Integer.toString(iPosicionX);
-                                        aBarcosNivel3Posiciones[1+(iBarcosNivel3Contador + 2)] = Integer.toString(iPosicionY + 1) + Integer.toString(iPosicionX);
-                                        aBarcosNivel3Posiciones[2+(iBarcosNivel3Contador + 2)] = Integer.toString(iPosicionY + 2) + Integer.toString(iPosicionX);
+                                        
+                                        try {
+                                            aBarcosNivel3Posiciones[0+(iBarcosNivel3Contador + 2)] = Integer.toString(iPosicionY) + Integer.toString(iPosicionX);
+                                            aBarcosNivel3Posiciones[1+(iBarcosNivel3Contador + 2)] = Integer.toString(iPosicionY + 1) + Integer.toString(iPosicionX);
+                                            aBarcosNivel3Posiciones[2+(iBarcosNivel3Contador + 2)] = Integer.toString(iPosicionY + 2) + Integer.toString(iPosicionX);  
+                                        } catch (Exception e) {
+                                            //TODO: handle exception
+                                            System.out.println("Error de posiciones");
+                                        }
+                                        
+                                        
                                     }
+                                    
+                                    System.out.println("cosa1");
+                                    iBarcosNivel3Contador++;
                                     break;
                                 }
                             }
@@ -159,20 +183,28 @@ public class cosas {
                                     cTablero[iPosicionX + 1][iPosicionY] = cBarco ;
                                     cTablero[iPosicionX][iPosicionY] = cBarco ;
                                     cTablero[iPosicionX  - 1][iPosicionY] = cBarco ;
-                                    iBarcosNivel3Contador++;
+                                    
 
                                     //Almacenar Posicion del barco de nivel 3 vertical
                                     //------------------------------------------------------------------------------------------
+                                    
                                     if (iBarcosNivel3Contador == 0) {
                                         aBarcosNivel3Posiciones[2] = Integer.toString(iPosicionY) + Integer.toString(iPosicionX + 1);
                                         aBarcosNivel3Posiciones[1] = Integer.toString(iPosicionY) + Integer.toString(iPosicionX);
                                         aBarcosNivel3Posiciones[0] = Integer.toString(iPosicionY) + Integer.toString(iPosicionX - 1);
                                     } else {
-                                        aBarcosNivel3Posiciones[0+(iBarcosNivel3Contador + 2)] = Integer.toString(iPosicionY) + Integer.toString(iPosicionX + 1);
-                                        aBarcosNivel3Posiciones[1+(iBarcosNivel3Contador + 2)] = Integer.toString(iPosicionY) + Integer.toString(iPosicionX);
-                                        aBarcosNivel3Posiciones[2+(iBarcosNivel3Contador + 2)] = Integer.toString(iPosicionY) + Integer.toString(iPosicionX - 1);
+                                        try {
+                                            aBarcosNivel3Posiciones[0+(iBarcosNivel3Contador + 2)] = Integer.toString(iPosicionY) + Integer.toString(iPosicionX + 1);
+                                            aBarcosNivel3Posiciones[1+(iBarcosNivel3Contador + 2)] = Integer.toString(iPosicionY) + Integer.toString(iPosicionX);
+                                            aBarcosNivel3Posiciones[2+(iBarcosNivel3Contador + 2)] = Integer.toString(iPosicionY) + Integer.toString(iPosicionX - 1);  
+                                        } catch (Exception e) {
+                                            //TODO: handle exception
+                                        }
+                                        
                                     }
-
+                                    
+                                    System.out.println("cosa2");
+                                    iBarcosNivel3Contador++;
                                     break;
                                 }
                                 
@@ -180,7 +212,6 @@ public class cosas {
 
                         }catch (Exception e) {
                         }
-                        
                     }
                 }
             }
@@ -210,17 +241,21 @@ public class cosas {
         do {
 
             if (iBarcosNivel3Undido != true) {
-                for (int i = 0; i < aBarcosNivel3Posiciones.length; i++) {
+                for (int i = 0; i < aBarcosNivel3Posiciones.length-3; i++) {
                     iPosicionY = Character.getNumericValue(aBarcosNivel3Posiciones[i].charAt(0));
                     iPosicionX = Character.getNumericValue(aBarcosNivel3Posiciones[i].charAt(1));
                     if (cTablero[iPosicionX][iPosicionY] == 'T') {
                         iBarcosTocadosNivel3++;
+                    } else {
+                        iBarcosTocadosNivel3 = 0;
                     }
                 }
-                if (iBarcosTocadosNivel3 == 4) {
-                    for (int i = 0; i < aBarcosNivel3Posiciones.length; i++) {
+                if (iBarcosTocadosNivel3 == aBarcosNivel3Posiciones.length-3) {
+                    for (int i = 0; i < aBarcosNivel3Posiciones.length-3; i++) {
                         iPosicionY = Character.getNumericValue(aBarcosNivel3Posiciones[i].charAt(0));
                         iPosicionX = Character.getNumericValue(aBarcosNivel3Posiciones[i].charAt(1));
+
+                        System.out.println("true1");
                        
                         cTablero[iPosicionX][iPosicionY] = 'H';
                         iBarcosNivel3Undido = true;
@@ -228,17 +263,47 @@ public class cosas {
                 }
             }
 
-            for (iPosicionX = 0; iPosicionX < cTablero.length; iPosicionX++) {
-                for (iPosicionY = 0; iPosicionY < cTablero.length; iPosicionY++) {
-                    if (cTablero[iPosicionX][iPosicionY] == cBarco) {
-                        System.out.print(" " + "*" + " ");
+            if (iBarcosNivel3Undido2 != true) {
+                for (int i = aBarcosNivel3Posiciones.length-3; i < aBarcosNivel3Posiciones.length; i++) {
+                    iPosicionY = Character.getNumericValue(aBarcosNivel3Posiciones[i].charAt(0));
+                    iPosicionX = Character.getNumericValue(aBarcosNivel3Posiciones[i].charAt(1));
+                    if (cTablero[iPosicionX][iPosicionY] == 'T') {
+                        iBarcosTocados2Nivel3++;
                     } else {
-                        System.out.print(" " + cTablero[iPosicionX][iPosicionY] + " ");
+                        iBarcosTocados2Nivel3 = 0;
                     }
-
                 }
-                System.out.println("");
+                if (iBarcosTocados2Nivel3 == aBarcosNivel3Posiciones.length-3) {
+                    for (int i = aBarcosNivel3Posiciones.length-3; i < aBarcosNivel3Posiciones.length; i++) {
+                        iPosicionY = Character.getNumericValue(aBarcosNivel3Posiciones[i].charAt(0));
+                        iPosicionX = Character.getNumericValue(aBarcosNivel3Posiciones[i].charAt(1));
+
+                        System.out.println("true2");
+                       
+                        cTablero[iPosicionX][iPosicionY] = 'H';
+                        iBarcosNivel3Undido2 = true;
+                    }
+                }
             }
+
+            System.out.println("    1  2  3  4  5  6  7  8  9  10");
+                for (iPosicionX = 0; iPosicionX < cTablero.length; iPosicionX++) {
+                    System.out.print(" " + Character.forDigit(iPosicionX + 10, 20) + " ");
+                    for (iPosicionY = 0; iPosicionY < cTablero.length; iPosicionY++) {
+
+                        if (cTablero[iPosicionX][iPosicionY] == cHundido) {
+                            System.out.print( GREEN+ " " + cTablero[iPosicionX][iPosicionY] + " " + RESET);
+                        } else if (cTablero[iPosicionX][iPosicionY] == cTocados) {
+                            System.out.print( RED + " " + cTablero[iPosicionX][iPosicionY] + " " + RESET);
+                        } else if (cTablero[iPosicionX][iPosicionY] == cBarco ) {
+                            System.out.print( BLUE + " " + cMar + " " + RESET);
+                        } else {
+                            System.out.print("[" + cTablero[iPosicionX][iPosicionY] + "]");
+                        }
+
+                    }
+                    System.out.println("");
+                }
 
             do {
 
